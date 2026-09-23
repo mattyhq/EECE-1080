b2c2657ad9b40a81e4c782753f1928d84e3325c4
matty <109246664+mattyhq@users.noreply.github.com>
2026-09-22 21:39:39 -0400

    Add charity charges calculation program

---
+++ ledgeon1.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+int main ()
+{
+
+const double charityChargesPerPatient = 1375.61;
+
+int firstYear = 0;
+int firstYearPatients = 0;
+int secondYear = 0;
+int secondYearPatients = 0;
+
+double firstYearCharityCharges = 0.0;
+double secondYearCharityCharges = 0.0;
+double patientIncreasePercent = 0.0;
+
+std::cout << "Enter the first yearz:";
+std::cin >> firstYear;
+
+std::cout << "Enter the number of patients we saw that year: ";
+std::cin >> firstYearPatients;
+
+std::cout << "Enter the second year: ";
+std::cin >> secondYear;
+
+std::cout << "Enter the number of patients we saw that year: ";
+std::cin >> secondYearPatients;
+
+firstYearCharityCharges =
+  firstYearPatients * charityChargesPerPatient;
+
+secondYearCharityCharges = 
+  secondYearPatients * charityChargesPerPatient;
+
+patientIncreasePercent = 
+(static_cast<double>(secondYearPatients - firstYearPatients) 
+/ firstYearPatients) * 100;
+
+std::cout << "In year " << firstYear
+              << ", our hospital issued "
+              << firstYearCharityCharges
+              << " dollars of charity charges.\n";
+
+std::cout << "In year " << secondYear
+              << ", our hospital issued "
+              << secondYearCharityCharges
+              << " dollars of charity charges.\n";
+
+std::cout << "Between " << firstYear
+              << " and " << secondYear
+              << ", there was a "
+              << patientIncreasePercent
+              << "% increase in patients seen at our hospital.\n";
+
+return 0;
+}
